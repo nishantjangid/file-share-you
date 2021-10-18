c0b59f810ce44aef12ede32e55d8f0c6e839335e
Nishant Jangid <57652095+nishantjangid@users.noreply.github.com>
2021-10-18 11:30:39 +0530

    update server.js for cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
